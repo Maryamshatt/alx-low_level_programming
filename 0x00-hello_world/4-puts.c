9d3a68aeab71986eee59710ac48be0382095aa10
Maryamshatt <maryamharuna98@gmail.com>
2023-07-13 08:54:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always o (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (o);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
