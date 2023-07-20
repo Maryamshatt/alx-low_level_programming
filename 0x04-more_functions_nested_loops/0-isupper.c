896149d6782a2b01ef442883b200c09e4f1a480d
maryamshatt <maryamharuna98@gmail.com>
2023-07-20 08:28:10 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
 * _isupper - check for uppercase character
 * @c: function parameter
 * Return: 1 on success 0 on failure
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#include "MAIN.H"
-#define "MAIN.H"
+#include "MAIN_H"
+#define "MAIN_H"
 
 int _isupper(int c);
 int mul(int a, int b);
