bc9197f8260cf97af38d5a8d4dda89e70600d21a
maryamshatt <maryamharuna98@gmail.com>
2023-07-20 08:14:17 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+* _isupper - check for uppercase character
+* @c: function parameter
+* Return: 1 on success 0 on failure
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}	
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#include "MAIN.H"
+#define "MAIN.H"
+
+int _isupper(int c);
+int mul(int a, int b);
+void print_numbers(void);
