ea2e21c4c817e33d07e3cd060c3c19c21c136f66
maryamshatt <maryamharuna98@gmail.com>
2023-08-04 07:18:31 -0700

    Static Librairies

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: the string to be searched
+* @needle: the substraing to be located
+*
+* Return: if the substring is not located - NULL
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+				index++;
+			} while (haystack[index] == needle[index]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
