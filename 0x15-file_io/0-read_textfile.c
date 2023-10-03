eebc66d67a0f3137e84aa04f12e69ccd4ecf19e1
maryamshatt <maryamharuna98@gmail.com>
2023-10-03 12:54:42 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,26 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+unsigned int binary_to_uint(const char *b)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int i;
+	unsigned int dec_val = 0;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	if (!b)
+i		return (0);
 
-	free(buf);
-	close(fd);
-	return (w);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
+
+	return (dec_val);
 }
 
